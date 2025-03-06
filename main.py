4ebb9c73946543255d9fe699c2d525602e8897c2
Robert-Debbas <147934154+Robert-Debbas@users.noreply.github.com>
2025-03-06 09:53:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ from itertools import product
 import copy
 from network import Layer 
 from network import Network
+from network import generate_network
+from network import generate_input_box
+from network import print_network_parameters
+from network import quantization_error
 from zonotope import Zonotope
 from constrained_zonotope import ConstrainedZonotope
+from oracle import sample_vectors_from_box
+from abstract import abstract_to_vertices
+
 
 ################################################################
 # CONSTRUCT NETWORKS                                           #             
 ################################################################
 
+dim = 3
+
+# structure = [dim, 4 , 3, 2, 1]
+
+# network = generate_network(structure)
 
 W0 = np.array([[1.25, -2.31, 4.52], [-3.20, 0.76, 2.12], [4.87, -1.40, 3.55]])
 b0 = np.array([[1.53], [-0.73], [2.39]])
@@ -43,32 +55,58 @@ quant_config = {
         "Cub_h": 100   # Upper bound for hidden layer activations 
     }
 
-quantized_network = intermediate_network.quantize(quant_config)
+quantized_network = intermediate_network.quantize(quant_config, version = 2)
 
 ################################################################
 # INPUT                                                        #
 ################################################################
 
+# r = 0.5
+
+# input_box = generate_input_box(dim, r)
+
+# input_box = np.array([
+#         [0.5, 1.5],  
+#         [1.5, 2.5],
+#         [-1.5, -0.5],
+#         [3.5, 4.5],
+#         [-3.5, -2.5]
+#     ])
+
 input_box = np.array([
         [0.5, 1.5],  
         [1.5, 2.5],
-        [-1.5, -0.5]
-    ])
+        [-1.5, -0.5]])
 
 ################################################################
 # TESTS                                                        #
 ################################################################
 
-output_box_1 = network.propagate_vertices(input_box)
-print(f"Propagation of vertices through NN : {output_box_1}")
+n = 10000000
 
-output_box_2 = network.propagate_zonotope(input_box)
-print(f"Propagation of zonotope through NN : {output_box_2}")
+# print_network_parameters(network)
 
-output_box_3 = quantized_network.propagate_vertices(input_box)
-print(f"Propagation of vertices through QNN : {output_box_3}")
+output_box_15 = network.propagate_vertices_random(input_box, n)
+print(f"Propagation of random vertices through NN:\n {output_box_15}")
 
-output_box_4 = quantized_network.propagate_zonotope(input_box)
-print(f"Propagation of zonotope through QNN : {output_box_4}")
+output_box_2, zonotopeNN = network.propagate_zonotope(input_box)
+print(f"Propagation of zonotope through NN:\n {output_box_2}")
+
+output_box_2C = network.propagate_constrained_zonotope(input_box)
+print(f"Propagation of constrained zonotope through NN:\n {output_box_2C}")
+
+# print_network_parameters(quantized_network)
+
+output_box_35 = quantized_network.propagate_vertices_random(input_box, n)
+print(f"Propagation of random vertices through QNN:\n {output_box_35}")
+
+output_box_4, zonotopeQNN = quantized_network.propagate_zonotope(input_box)
+print(f"Propagation of zonotope through QNN:\n {output_box_4}")
+
+output_box_4C = quantized_network.propagate_constrained_zonotope(input_box)
+print(f"Propagation of constrained zonotope through QNN:\n {output_box_4C}")
+
+quantization_error_zonotope = quantization_error(zonotopeNN, zonotopeQNN, dim)
+print(f"Quantization error zonotope:\nWeight:{quantization_error_zonotope.W}\nbias:{quantization_error_zonotope.b}")
 
 
