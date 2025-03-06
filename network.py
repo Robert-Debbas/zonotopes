ef30c41fc30801d03483edbbebf03e59369570d5
Robert-Debbas <147934154+Robert-Debbas@users.noreply.github.com>
2025-03-06 09:54:18 +0100

    Update network.py

--- network.py
+++ network.py
@@ -5,8 +5,14 @@ from zonotope import Zonotope
 from constrained_zonotope import ConstrainedZonotope
 from abstract import clamp
 from abstract import abstract_to_vertices
+from abstract import abstract_to_constrained_zonotope
 from abstract import concretize_to_box
 from abstract import abstract_to_zonotope
+from abstract import relu_interval_propagation
+from abstract import linear_interval_propagation
+from abstract import clamp_interval_propagation
+from oracle import sample_vectors_from_box
+
 
 class Layer:
 
@@ -23,15 +29,33 @@ class Layer:
         if lower_bound: self.lower_bound = lower_bound
         if upper_bound: self.upper_bound = upper_bound
 
+    def propagate(self, vector):
+
+        vector = (self.weights @ vector.T + self.biases).T
+
+        if self.activation == 'relu': vector = np.maximum(0, vector)
+        elif self.activation == 'clamp': vector = clamp(np.round(vector), self.lower_bound, self.upper_bound)
+
+        return vector
+
+    def propagate_box(self, box):
+
+        box = linear_interval_propagation(box, self.weights, self.biases)
+
+        if self.activation == 'relu': box = relu_interval_propagation(box)
+        elif self.activation == 'clamp': box = clamp_interval_propagation(np.round(box), self.lower_bound, self.upper_bound)
+
+        return box
+
     def propagate_vertices(self, vertices):
         
         vertices = np.dot(self.weights, vertices.T).T + self.biases.T
 
         if self.activation == 'relu': vertices = np.maximum(0, vertices)
-        elif self.activation == 'clamp': vertices = np.minimum(np.maximum(np.round(vertices), self.lower_bound), self.upper_bound)
+        elif self.activation == 'clamp': vertices = clamp(np.round(vertices), self.lower_bound, self.upper_bound)
 
         return vertices
-        
+
     def propagate_zonotope(self, zonotope):
         
         zonotope = zonotope.linear_transformation(self.weights, self.biases)
@@ -67,7 +91,7 @@ class Network:
 
         self.quantized = False
 
-    def quantize(self, configuration):
+    def quantize(self, configuration, version):
 
         self.quantized = True 
 
@@ -77,35 +101,56 @@ class Network:
         Clb_b, Cub_b = configuration["Clb_b"], configuration["Cub_b"]
         Cub_h = configuration["Cub_h"]
 
-        self.layers[0].weights = clamp(np.round(self.layers[0].weights * (2**Fw)), Clb_w, Cub_w)
-        self.layers[0].weights = (2**(Fh - Fw - Fin)) * self.layers[0].weights
+        if version == 1: 
+            exponent1 = Fh - Fw - Fin
+            exponent2 = Fh - Fb
+        else: 
+            exponent1 = - Fw - Fin
+            exponent2 = - Fb
 
-        self.layers[0].biases = clamp(np.round(self.layers[0].biases * (2**Fb)), Clb_b, Cub_b)
-        self.layers[0].biases = (2**(Fh - Fb)) * self.layers[0].biases
+
+        self.layers[0].weights = clamp(np.round(self.layers[0].weights * (2 ** Fw)), Clb_w, Cub_w)
+        self.layers[0].weights = (2 ** (exponent1)) * self.layers[0].weights
+
+        self.layers[0].biases = clamp(np.round(self.layers[0].biases * (2 ** Fb)), Clb_b, Cub_b)
+        self.layers[0].biases = (2 ** (exponent2)) * self.layers[0].biases
 
         if self.layers[0].activation == 'relu': 
                 self.layers[0].activation = 'clamp'
                 self.layers[0].lower_bound = 0
                 self.layers[0].upper_bound = Cub_h
 
+        if version == 1: 
+            exponent1 = Fh - Fw 
+            exponent2 = Fh - Fb
+        else: 
+            exponent1 = - Fw 
+            exponent2 = - Fb
 
-        for i in range(1, len(self.layers)-1):
+        for i in range(1, len(self.layers) - 1):
 
             self.layers[i].weights = clamp(np.round(self.layers[i].weights * (2**Fw)), Clb_w, Cub_w)
-            self.layers[i].weights = (2**(Fh - Fw)) * self.layers[i].weights
+            self.layers[i].weights = (2**(exponent1)) * self.layers[i].weights
             self.layers[i].biases = clamp(np.round(self.layers[i].biases * (2**Fb)), Clb_b, Cub_b)
-            self.layers[i].biases = (2**(Fh - Fb)) * self.layers[i].biases
+            self.layers[i].biases = (2**(exponent2)) * self.layers[i].biases
 
             if self.layers[i].activation == 'relu': 
                 self.layers[i].activation = 'clamp'
                 self.layers[i].lower_bound = 0
                 self.layers[i].upper_bound = Cub_h
 
+        if version == 1: 
+            exponent1 = - Fw - ( len(self.layers) - 2) * Fh
+            exponent2 = - Fb - ( len(self.layers) - 2) * Fh
+        else: 
+            exponent1 = - Fw 
+            exponent2 = - Fb
+
         self.layers[-1].weights = clamp(np.round(self.layers[-1].weights * (2**Fw)), Clb_w, Cub_w)
-        self.layers[-1].weights = (2**(- Fw - ( len(self.layers) - 1) * Fh)) * self.layers[-1].weights
+        self.layers[-1].weights = (2**(exponent1)) * self.layers[-1].weights
 
         self.layers[-1].biases = clamp(np.round(self.layers[-1].biases * (2**Fb)), Clb_b, Cub_b)
-        self.layers[-1].biases = (2**(- Fb -( len(self.layers) - 1) * Fh)) * self.layers[-1].biases
+        self.layers[-1].biases = (2**(exponent2)) * self.layers[-1].biases
 
         if self.layers[-1].activation == 'relu': 
                 self.layers[-1].activation = 'clamp'
@@ -120,12 +165,31 @@ class Network:
         self.layers = [input_layer] + self.layers
         
         return self
-     
+
+    def propagate(self, vector):
+        
+        for i in range(len(self.layers)): vector = self.layers[i].propagate(vector)
+
+        return vector.T
+
+    def propagate_box(self, box):
+
+        for i in range(len(self.layers)): box = self.layers[i].propagate_box(box)
+
+        return box
+
     def propagate_vertices(self, box):
 
         vertices = abstract_to_vertices(box)
-        for i in range(len(self.layers)):
-            vertices = self.layers[i].propagate_vertices(vertices)
+        for i in range(len(self.layers)): vertices = self.layers[i].propagate_vertices(vertices)
+        out_box = concretize_to_box(vertices)
+
+        return out_box
+     
+    def propagate_vertices_random(self, box, n):
+
+        vertices = np.vstack((abstract_to_vertices(box), sample_vectors_from_box(box,n)))
+        for i in range(len(self.layers)): vertices = self.layers[i].propagate_vertices(vertices)
         out_box = concretize_to_box(vertices)
 
         return out_box
@@ -148,27 +212,103 @@ class Network:
 
         out_box = zonotope.concretize()
 
-        return out_box
+        return out_box, zonotope
     
-    def propagate_constrained_zonotope(self, czonotope):
+    def propagate_constrained_zonotope(self, box):
         
         start = 0
 
         if self.quantized == True:
 
             vertices = abstract_to_vertices(box)
-            vertices = self.layer[0].propagate_vertices(vertices)
+            vertices = self.layers[0].propagate_vertices(vertices)
             box = concretize_to_box(vertices)
 
             start = 1 
 
         czonotope = abstract_to_constrained_zonotope(box)
 
-        for i in range(start, len(self.layers)): czonotope = self.layer[i].propagate_constrained_zonotope(czonotope)
+        for i in range(start, len(self.layers)): czonotope = self.layers[i].propagate_constrained_zonotope(czonotope)
 
-        out_box = czonotope.concretize()
+        out_box = czonotope.final_concretize()
 
         return out_box
+
+def generate_network(structure):
+    """
+    Generates a deep forward neural network with ReLU activations in all layers
+    except for the output layer, which has an identity activation.
+
+    :param structure: List representing the number of neurons in each layer
+                      e.g., [2, 2, 1] -> Input layer with 2 neurons, one hidden layer with 2 neurons, output layer with 1 neuron
+    :return: A Network object
+    """
+
+    layers = []
+
+    for i in range(len(structure) - 1):
+
+        W = np.random.uniform(-5, 5, (structure[i + 1], structure[i])) 
+        b = np.random.uniform(-5, 5, (structure[i + 1], 1))
+        activation = 'relu' if i < len(structure) - 2 else 'id' 
+        layers.append(Layer(W, b, activation))
+
+    return Network(layers)
+
+def generate_input_box(dim, r):
+    """
+    Generates a random dim-dimensional input box centered around a randomly generated point.
+    The box is constructed with a given radius r.
+
+    :param dim: Dimension of the input box
+    :param r: Radius of the box
+    :return: A NumPy array of shape (dim, 2) representing the input box
+    """
+    center = np.random.uniform(-5, 5, dim)  # Random center point within [-5, 5]
+    
+    # Compute lower and upper bounds based on radius r
+    lower_bounds = center - r  
+    upper_bounds = center + r
+
+    # Format as a NumPy array with shape (dim, 2)
+    input_box = np.column_stack((lower_bounds, upper_bounds))
+    
+    return input_box
+
+def print_network_parameters(network):
+    """
+    Prints the weights and biases of a given Network object.
+
+    :param network: A Network object containing layers with weights and biases
+    """
+    for idx, layer in enumerate(network.layers):
+        print(f"Layer {idx}:")
+        print("Weights:\n", layer.weights)
+        print("Biases:\n", layer.biases)
+        print("Activation:", layer.activation)
+        print("-" * 40)
+
+
+def quantization_error(zonotopeNN, zonotopeQNN, dim_input):
+
+    W, b = zonotopeNN.W, zonotopeNN.b
+    Wq, bq = zonotopeQNN.W, zonotopeQNN.b
+    We, be = np.zeros((len(Wq), len(Wq[0]))), np.zeros_like(zonotopeQNN.b)
+
+    WT = W.T
+    WqT = Wq.T
+    WeT = We.T
+
+    for i in range(dim_input): WeT[i] = WT[i] - WqT[i]
+    for j in range(dim_input, len(WeT)): 
+        print(WT, dim_input + (j - dim_input)//2)
+        if (j - dim_input) % 2 == 0: WeT[j] = WqT[j]
+        else: WeT[j] = WT[dim_input + (j - dim_input)//2 - 1] - WqT[j]
+
+    We = WeT.T
+    be = b - bq
+
+    return Zonotope(We, be)
 
     
 
